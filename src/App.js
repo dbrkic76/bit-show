62dae1dc70961751fbb1ef1e096ab9e30513227f
dbrkic76 <brkicdragan@hotmail.com>
2024-11-25 17:13:43 +0100

    bit-show

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [show, setShow] = useState(null);
+
+  const fetchSelectedShow = useCallback(async () => {
+    if (selectedOption) {
+      const url = `https://api.tvmaze.com/shows/${selectedOption}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setShow(data);
+    }
+  }, [selectedOption]);
+
+  const fetchSearchedShows = async (query) => {
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${query}`
+    );
+    const data = await response.json();
+    setOptions(data);
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchQuery(value);
+    if (value) {
+      fetchSearchedShows(value);
+    } else {
+      setOptions([]);
+    }
+  };
+
+  const handleSelectChange = (event) => {
+    const selectedID = event.target.value;
+
+    setSelectedOption(selectedID);
+    fetchSelectedShow(selectedOption);
+    console.log(selectedID);
+  };
+
+  useEffect(() => {
+    if (selectedOption) {
+      fetchSelectedShow(selectedOption);
+    }
+  }, [selectedOption]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        onInput={(event) => handleInputChange(event)}
+        value={searchQuery}
+      />
+
+      {options.length > 0 && (
+        <select onChange={handleSelectChange}>
+          {options?.map((show) => {
+            return (
+              <option key={show.show.id} value={show.show.id}>
+                {show.show.name}
+              </option>
+            );
+          })}
+        </select>
+      )}
+      {show && (
+        <div>
+          <p>{show.name}</p>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
