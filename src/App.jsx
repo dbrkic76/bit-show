a732e44345017815d1c8eb6953a0180f1481caf9
dbrkic76 <brkicdragan@hotmail.com>
2024-12-02 17:19:35 +0100

    vezbe

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import React, { useState, useEffect } from "react";
+import HomePage from "./components/HomePage";
+import SingleShowPage from "./components/SingleShowPage";
+import { Routes, Route, Navigate } from "react-router";
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<>
+        <Header />
+        <HomePage />
+        <Footer />
+      </>} />
+      <Route path="single-show/:id" element={<>
+        <Header />
+        <SingleShowPage />
+        <Footer />
+      </>} />
+    </Routes>
+  );
+};
+export default App;
--- src/components/SingleShowPage.jsx
+++ src/components/SingleShowPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const SingleShowCard = ({ selectedShow, setSelectedShow }) => {
+const SingleShowPage = ({ selectedShow, setSelectedShow }) => {
   return <div className="SingleShowCard">SingleShowCard</div>;
 };
 
-export default SingleShowCard;
+export default SingleShowPage;
